8bd44491cc989d14034088b04e69e92753fb5c72
David Wolfe <davidgameswolfe@gmail.com>
2014-05-31 22:16:39 -0300

    Add aws_policy_manager optionally

--- aws_policy_manager/urls.py
+++ aws_policy_manager/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from aws_policy_manager.views import ListAWSPoliciesView
 
-aws_patterns = patterns('aws_policy_manager.views',
+urlpatterns = patterns(
+    'aws_policy_manager.views',
     url(r'^$', ListAWSPoliciesView.as_view(), name='policy_list'),
 )
